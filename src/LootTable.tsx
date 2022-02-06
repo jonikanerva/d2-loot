33507d89549b602c0cc769a3649c8fa8982fe51e
Joni Kanerva <jhonny@jhonny.org>
2022-02-06 09:40:03 +0200

    remove dropdown, just show all

--- src/LootTable.tsx
+++ src/LootTable.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react'
-import { fetchData, Item } from './bungieData'
-import SourceDropdown from './SourceDropdown'
-import Weapon from './Weapon'
 import './LootTable.css'
+import React, { Fragment, useEffect, useState } from 'react'
+import { fetchData, Item } from './bungieData'
+import Header from './Header'
+import Weapon from './Weapon'
 
 const LootTable: React.FC = () => {
+  let previousTitle = ''
   const [loot, setLoot] = useState<Item[]>()
-  const [source, setSource] = useState<string>(
-    'Complete strikes and earn rank-up packages from Commander Zavala.'
-  )
 
   useEffect(() => {
     fetchData().then((loot) => setLoot(loot))
@@ -18,24 +16,19 @@ const LootTable: React.FC = () => {
     return <h1>Loading...</h1>
   }
 
-  const data = loot.filter((loot) => loot.source === source)
-  const sourceList = [...new Set(loot.map((item) => item.source))].sort(
-    (a, b) => (a > b ? 1 : -1)
-  )
-
   return (
-    <div>
-      <SourceDropdown
-        values={sourceList}
-        defaultValue={source}
-        onChangeFunction={(event) => setSource(event.target.value)}
-      />
-      <h3>{data[0]?.source}</h3>
-      <div className="lootTable">
-        {data.map((item, key) => (
-          <Weapon item={item} key={key} />
-        ))}
-      </div>
+    <div className="lootTable">
+      {loot.map((item, key) => {
+        const titleChanged = previousTitle !== item.source
+        previousTitle = item.source
+
+        return (
+          <Fragment key={key}>
+            {titleChanged && <Header title={item.source} />}
+            <Weapon item={item} />
+          </Fragment>
+        )
+      })}
     </div>
   )
 }
